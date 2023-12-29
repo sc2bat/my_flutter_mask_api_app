e47abd4e79eb9b796ddf143e04bd744d3151700e
stw26 <it.stw26@contentlap.com>
2023-12-29 11:28:58 +0900

    create remainstat widget

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -16,17 +17,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<StoreModel> stores = [];
 
+  bool _isLoading = true;
+
   Future fetch() async {
+    _isLoading = true;
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
     final response = await http.get(Uri.parse(url));
     final jsonStores = jsonDecode(utf8.decode(response.bodyBytes))['stores'];
+    setState(() {
+      stores.clear();
 
-    stores.clear();
-
-    jsonStores.forEach((e) {
-      stores.add(StoreModel.fromJson(e));
+      jsonStores.forEach((e) {
+        stores.add(StoreModel.fromJson(e));
+      });
+      _isLoading = false;
     });
+    logger.info('qwerasdf fetch');
+    logger.info('qwerasdf loading $_isLoading');
   }
 
   @override
@@ -41,14 +49,80 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('마스크 재고 있는 곳 : 00'),
+        title: Text('마스크 재고 있는 곳 : ${stores.length}'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              logger.info('qwerasdf press button');
+              fetch();
+              logger.info('qwerasdf press button');
+            },
+          ),
+        ],
       ),
-      body: FloatingActionButton(onPressed: () {
-        fetch();
-      }),
-      //  ListView(
-      //   children: stores.map((e) => Text(e.remainStat ?? '')).toList(),
-      // ),
+      body: _isLoading
+          ? loadingWidget()
+          : ListView(
+              children: stores.map((e) {
+                return ListTile(
+                  title: Text(e.name),
+                  subtitle: Text(e.addr),
+                  trailing: _buildRemainStatWidget(e),
+                );
+              }).toList(),
+            ),
+    );
+  }
+
+  Widget loadingWidget() {
+    return const Center(
+      child: Column(
+        children: [
+          Text('isLoading'),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(StoreModel store) {
+    var remainStat = '판매중지';
+    var description = '';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+      case 'some':
+        remainStat = '보통';
+        description = '30개 이상 100개 미만';
+        color = Colors.yellow;
+      case 'few':
+        remainStat = '부족';
+        description = '2개 이상 30개 미만';
+        color = Colors.red;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+
+      default:
+    }
+
+    return Column(
+      children: [
+        Text(
+          remainStat,
+          style: TextStyle(fontWeight: FontWeight.bold, color: color),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
     );
   }
 }
