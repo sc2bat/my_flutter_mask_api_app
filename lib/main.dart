ccec22488f36aa79f3e7c4842311c12d8085a8ae
stw26 <it.stw26@contentlap.com>
2023-12-29 10:25:48 +0900

    get json data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_flutter_mask_api_app/logger/logger.dart';
+import 'package:my_flutter_mask_api_app/model/store_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,15 +35,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final stores = List<StoreModel>;
+
+  Future fetch() async {
+    var url =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
+    final response = await http.get(Uri.parse(url));
+    final jsonStores = jsonDecode(utf8.decode(response.bodyBytes));
+
+    jsonStores.forEach((e) {
+      StoreModel.fromJson(e);
+    });
+    // stores.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('마스크 재고 있는 곳 : 00'),
+      ),
+      body: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            fetch();
+          },
         ),
-        body: const Center(
-          child: Text('qwerasdf'),
-        ));
+      ),
+    );
   }
 }
