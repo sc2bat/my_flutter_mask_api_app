280e323850d128115376bb6f6c902ea9113c913a
stw26 <it.stw26@contentlap.com>
2023-12-29 10:56:09 +0900

    check api docuement

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -20,22 +20,17 @@ class _MyHomePageState extends State<MyHomePage> {
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
     final response = await http.get(Uri.parse(url));
-    logger.info('qwerasdf fetch 111');
-    final jsonStores = jsonDecode(utf8.decode(response.bodyBytes));
-    logger.info('qwerasdf fetch 222');
+    final jsonStores = jsonDecode(utf8.decode(response.bodyBytes))['stores'];
 
     stores.clear();
-    logger.info('qwerasdf fetch ${stores.length}');
-    // logger.info('qwerasdf ${jsonStores['stores']}');
+
     jsonStores.forEach((e) {
       stores.add(StoreModel.fromJson(e));
     });
-    logger.info('qwerasdf fetch ${stores.length}');
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     fetch();
     logger.info('qwerasdf init done');
@@ -44,12 +39,16 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('마스크 재고 있는 곳 : 00'),
-        ),
-        body: ListView(
-          children: stores.map((e) => Text(e.remainStat ?? '')).toList(),
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('마스크 재고 있는 곳 : 00'),
+      ),
+      body: FloatingActionButton(onPressed: () {
+        fetch();
+      }),
+      //  ListView(
+      //   children: stores.map((e) => Text(e.remainStat ?? '')).toList(),
+      // ),
+    );
   }
 }
